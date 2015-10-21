b58319b677074c894efdfc159ccc4daf08467116
Kevin Lin <whatakevin@gmail.com>
2015-10-21 01:05:35 -0500

    Remove hash from URL, refactor angular routeProvider logic to app.js

--- idb/static/js/app.js
+++ idb/static/js/app.js
@@ -6,8 +6,31 @@ angular.module('tbsaApp', [
     'tbsaApp.investor'
     ])
 
-.config(['$routeProvider', function($routeProvider){
-    $routeProvider.otherwise({redirectTo: '/'});
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: '/static/partials/home.html',
+            controller: 'homeCtrl',
+            controllerAs: 'home'
+        })
+        .when('/companies', {
+            templateUrl: '/static/partials/company.html',
+            controller: 'companyCtrl',
+            controllerAs: 'company'
+        })
+        .when('/founders', {
+            templateUrl: '/static/partials/founder.html',
+            controller: 'founderCtrl',
+            controllerAs: 'founder'
+        })
+        .when('/investors', {
+            templateUrl: '/static/partials/investor.html',
+            controller: 'investorCtrl',
+            controllerAs: 'investor'
+        })
+        .otherwise({redirectTo: '/'});
+        
+    $locationProvider.html5Mode(true);
 }])
 
 .controller('indexCtrl', ['$scope', function($scope){
--- idb/static/js/home.js
+++ idb/static/js/home.js
@@ -1,13 +1,5 @@
 angular.module('tbsaApp.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: '/static/partials/home.html',
-        controller: 'homeCtrl',
-        controllerAs: 'home'
-    });
-}])
-
 .controller('homeCtrl', [function(){
     this.greeting = "konichiwa";
 }]);
