d6e433e876031ba1c4a10d5b3f0de3ab594a2de1
Kevin Lin <whatakevin@gmail.com>
2015-10-22 18:46:37 -0500

    Champion table sortable in both directions

--- idb/static/js/champion.js
+++ idb/static/js/champion.js
@@ -5,4 +5,14 @@ angular.module('tbsaApp.champion', ['ngRoute'])
                       {name: "Teemo",    id: 2, description: "The Swift Scout"},
                       {name: "Renekton", id: 3, description: "The Butcher of the Sands"}];
     this.order = "id";
+    this.orderReverse = false;
+    this.changeOrderTo = function(newOrder) {
+        if(this.order === newOrder){
+            this.orderReverse = ! this.orderReverse;
+        }
+        else {
+            this.orderReverse = false;
+        }
+        this.order = newOrder;
+    };
 }]);
