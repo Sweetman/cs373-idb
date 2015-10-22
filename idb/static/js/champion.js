97aed943a0bb694f206425bb71c51fb39954dec0
Kevin Lin <whatakevin@gmail.com>
2015-10-22 05:20:56 -0500

    Sortable champion table

--- idb/static/js/champion.js
+++ idb/static/js/champion.js
@@ -4,4 +4,5 @@ angular.module('tbsaApp.champion', ['ngRoute'])
     this.tableData = [{name: "Lee Sin",  id: 1, description: "The Blind Monk"},
                       {name: "Teemo",    id: 2, description: "The Swift Scout"},
                       {name: "Renekton", id: 3, description: "The Butcher of the Sands"}];
+    this.order = "id";
 }]);
