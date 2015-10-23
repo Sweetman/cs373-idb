3d25fda9f47ed0c48b27ca3a765b1099096038f3
Kevin Lin <whatakevin@gmail.com>
2015-10-22 20:53:40 -0500

    Populate about page

--- idb/static/js/about.js
+++ idb/static/js/about.js
@@ -1,9 +1,9 @@
 angular.module('tbsaApp.about', ['ngRoute'])
 
 .controller('aboutCtrl', [function(){
-    this.teamMembers = [{id: 1, name: "Kevin Lin"},
-                        {id: 2, name: "James Sweetman"},
-                        {id: 3, name: "Hanah Luong"},
-                        {id: 4, name: "Jeffrey Li"},
-                        {id: 5, name: "Clinton Burgos"}];
+    this.teamMembers = [{id: 1, name: "Kevin Lin", commits: 0, issues: 0, unitTests: 0, bio: "lorem ipsum"},
+                        {id: 2, name: "James Sweetman", commits: 0, issues: 0, unitTests: 0, bio: "lorem ipsum"},
+                        {id: 3, name: "Hanah Luong", commits: 0, issues: 0, unitTests: 0, bio: "lorem ipsum"},
+                        {id: 4, name: "Jeffrey Li", commits: 0, issues: 0, unitTests: 0, bio: "lorem ipsum"},
+                        {id: 5, name: "Clinton Burgos", commits: 0, issues: 0, unitTests: 0, bio: "lorem ipsum"}];
 }]);
