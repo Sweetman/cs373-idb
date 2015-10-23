aa4ef2b3bc38e4dc7f76e2ec469991f9a8b82b27
Jeffrey Li <lijeffreytli@gmail.com>
2015-10-22 20:33:00 -0500

    populating table data

--- idb/static/js/champion.js
+++ idb/static/js/champion.js
@@ -1,9 +1,9 @@
 angular.module('tbsaApp.champion', ['ngRoute'])
 
 .controller('championCtrl', [function(){
-    this.tableData = [{name: "Lee Sin",  id: 1, description: "The Blind Monk"},
-                      {name: "Teemo",    id: 2, description: "The Swift Scout"},
-                      {name: "Renekton", id: 3, description: "The Butcher of the Sands"}];
+    this.tableData = [{name: "Lee Sin",  id: 1, description: "The Blind Monk", attack: "8", defense: "5", difficulty: "6", magic: "3"},
+                      {name: "Teemo",    id: 2, description: "The Swift Scout", attack: "5", defense: "3", difficulty: "6", magic: "7"},
+                      {name: "Renekton", id: 3, description: "The Butcher of the Sands", attack: "8", defense: "5", difficulty: "3", magic: "2"}];
     this.order = "id";
     this.orderReverse = false;
     this.changeOrderTo = function(newOrder) {
