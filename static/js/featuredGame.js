9b36869323722e641da9ebf5dc0d70bd69012947
Kevin Lin <whatakevin@gmail.com>
2015-11-05 19:31:48 -0800

    get featuredGame page working

--- static/js/featuredGame.js
+++ static/js/featuredGame.js
@@ -2,8 +2,8 @@ angular.module('hardcarryApp.featuredGame', ['ngRoute'])
 
 .controller('featuredGameCtrl', ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http){
     this.id = $routeParams.id;
-    $http.get("/api/featuredGames/" + id)
+    $http.get("/api/featured-games/" + this.id)
         .success(function(response){
-            $scope.champion.data = response;
+            $scope.featuredGame.data = response;
         });
 }]);
