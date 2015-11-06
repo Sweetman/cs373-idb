2f2584fc7ffd7b3cb86c4fabffdef0ac8b579a18
Kevin Lin <whatakevin@gmail.com>
2015-11-05 19:25:28 -0800

    Get featuredGameTable working

--- static/js/featuredGameTable.js
+++ static/js/featuredGameTable.js
@@ -1,9 +1,10 @@
 angular.module('hardcarryApp.featuredGameTable', [])
 
-.controller('featuredGameTableCtrl', ['$scope', function($scope){
-    this.tableData = [{name: "Lich Bane",         id: 1, cost: 3000, baseCost: 200, nameId: 212},
-                      {name: "The Bloodthirster", id: 2, cost: 3500, baseCost: 200, nameId: 123},
-                      {name: "Frozen Heart",      id: 3, cost: 2450, baseCost: 600, nameId: 234}];
+.controller('featuredGameTableCtrl', ['$scope', "$http", function($scope, $http){
+    $http.get("/api/featured-games")
+        .success(function(response){
+            $scope.featuredGameTable.tableData = response;
+        });
     this.order = "id";
     this.orderReverse = false;
     this.changeOrderTo = function(newOrder) {
