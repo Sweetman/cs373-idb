b08e5db56b46416d39eff46e6e20b2adf962e06b
Kevin Lin <whatakevin@gmail.com>
2015-11-06 19:13:56 -0800

    champion, summoner, and featuredGame retrieve all rest data

--- static/js/summoner.js
+++ static/js/summoner.js
@@ -6,4 +6,12 @@ angular.module('hardcarryApp.summoner', ['ngRoute'])
         .success(function(response){
             $scope.summoner.data = response;
         });
+    $http.get("/api/summoners/" + this.id + "/champions")
+        .success(function(response){
+            $scope.summoner.champions = response;
+        });
+    $http.get("/api/summoners/" + this.id + "/featured-games")
+        .success(function(response){
+            $scope.summoner.featuredGames = response;
+        });
 }]);
