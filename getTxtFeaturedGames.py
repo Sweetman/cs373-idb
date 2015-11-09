2c9bd115057dfa3a2683d9cb8098065777b7eae7
James Sweetman <sweetman@utexas.edu>
2015-11-09 17:13:12 -0600

    updated scrapers

---
+++ getTxtFeaturedGames.py
@@ -0,0 +1,12 @@
+from getFeaturedGames import add_games
+import json, os
+
+if __name__ == '__main__':
+	path = "%s/FeaturedGamesScraper" % (os.getcwd())
+	os.chdir(path)
+	jsonResponse = ""
+	for i in os.listdir(os.getcwd()):
+		if i.endswith(".txt"):
+			f = open(i, "r")
+			jsonResponse = json.load(f)
+			add_games(jsonResponse)
