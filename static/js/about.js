328bd3300b02335d80a16c9690674e0754f2ac86
Kevin Lin <whatakevin@gmail.com>
2015-11-13 01:58:20 -0600

    Refine resizing of About cards

--- static/js/about.js
+++ static/js/about.js
@@ -1,7 +1,7 @@
 angular.module('hardcarryApp.about', ['ngRoute'])
 
 .controller('aboutCtrl', [function(){
-    this.teamMembers = [{id: 1, name: "Kevin Lin", commits: 60, issues: 0, responsibility: "Front-End", unitTests: 0, bio: "Kevin enjoys biking and having fun in the sun!"},
+    this.teamMembers = [{id: 1, name: "Kevin Lin", commits: 60, issues: 0, responsibility: "Javascript", unitTests: 0, bio: "Kevin enjoys biking and having fun in the sun!"},
                         {id: 2, name: "James Sweetman", commits: 29, issues: 0, responsibility: "Back-End", unitTests: 0, bio: "James is an aesthetic god who puts the sweet in Sweetman."},
                         {id: 3, name: "Hanah Luong", commits: 44, issues: 4, responsibility: "Front-End", unitTests: 0, bio: "Hanah hates long walks in the park, flowers, and ugly cats."},
                         {id: 4, name: "Jeffrey Li", commits: 63, issues: 31, responsibility: "Full-Stack", unitTests: 0, bio: "Jeffrey enjoys long walks in the park, flowers, and cute dogs."},
